efbb4e6a2b55969cf9af9003333786c40b92da0e
lftimm <lftimm1902@gmail.com>
2026-04-22 21:35:11 -0300

    FINISHED INTEGRATION TEST 1D BAR

--- tests/integration/oneD_bar.cpp
+++ tests/integration/oneD_bar.cpp
@@ -13,21 +13,30 @@ Publisher: Orsa Maggiore
 using namespace cardozo;
 
 TEST(FiniteElement, ONE_D_BAR) {
+    using NodeINodeJ=std::pair<int,int>;
+    using ElasticityArea=std::pair<float,float>;
 
-    std::array<float,4> coords{0,50,70,90};
+    // Initial Data
+    std::array<float,4> coords{
+        0,
+        50,
+        70,
+        90
+    };
 
     std::array<std::pair<int,int>,3> eles{
-        std::pair<int,int>{0,1},
-        std::pair<int,int>{1,2},
-        std::pair<int,int>{2,3},
+        NodeINodeJ{0,1},
+        NodeINodeJ{1,2},
+        NodeINodeJ{2,3},
     };
 
     std::array<std::pair<float,float>,3> props{
-        std::pair<float,float>{2e5,5},
-        std::pair<float,float>{2e5,10},
-        std::pair<float,float>{2e5,10},
+        ElasticityArea{2e5,5},
+        ElasticityArea{2e5,10},
+        ElasticityArea{2e5,10},
     };
 
+    // Creating Elements
     Matrix2x2 element1 {
         {1,-1},
         {-1,1}
@@ -46,32 +55,41 @@ TEST(FiniteElement, ONE_D_BAR) {
     };
     element3 = element3 * (props[2].first*props[2].second/(coords[3]-coords[2]));
     
-    std::array<Matrix2x2,3> localMtx{element1,element2,element3};
+    std::array<Matrix2x2,3> elementData{element1,element2,element3};
 
-    Matrix4x4 globalMatrix{};
+    // Creating Global Matrix
+    Matrix4x4 K{};
 
-    for(size_t e = 0; e < eles.size(); e++) {
-        int dofs[2] = {eles[e].first, eles[e].second};
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < 2; j++) {
-                globalMatrix(dofs[i], dofs[j]) += localMtx[e](i, j);
-            }
-        }
+    for(int e = 0; e < elementData.size(); ++e) {
+        int nodeI = eles[e].first;
+        int nodeJ = eles[e].second;
+
+        K(nodeI, nodeI) += elementData[e](0, 0);
+        K(nodeI, nodeJ) += elementData[e](0, 1);
+        K(nodeJ, nodeI) += elementData[e](1, 0);
+        K(nodeJ, nodeJ) += elementData[e](1, 1);
     }
 
-    Vector B{4};
-    B(3) = 10000.0; 
+    // Displacement and Forces Vectors
+    Vector U{4};
+    U(0) = 0;
 
-    for (int i = 0; i < 4; i++) {
-        globalMatrix(0, i) = 0.0;
-        globalMatrix(i, 0) = 0.0;
+    Vector F{4};
+    F(3) = 500;
+
+    // Boundary conditions
+    for(int i = 0; i < 3; i++)
+    {
+        K(0, i) = 0;
+        K(i, 0) = 0;
     }
-    globalMatrix(0, 0) = 1.0;
-    B(0) = 0.0; 
 
-    Vector x{4}; 
-    
-    EXPECT_NO_THROW(algos::conjugateGradient(x, globalMatrix, B));
-    
-    EXPECT_GT(x(3), 0.0);
+    // Solve
+    Vector u = algos::conjugateGradient(U, K, F);
+    std::cerr << u; 
+
+    EXPECT_FLOAT_EQ(u(0),0);
+    EXPECT_FLOAT_EQ(u(1),0.025f);
+    EXPECT_FLOAT_EQ(u(2),0.030f);
+    EXPECT_FLOAT_EQ(u(3),0.035f);
 }
